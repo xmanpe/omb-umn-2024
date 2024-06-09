f897d20bc28fe3242da76bd43dc892caa7ad12dc
Axel Ferdinand <axel.ferdinand.88@gmail.com>
2024-06-09 17:41:50 +0700

    solve senggolan

--- src/components/petugas-upacara/PetugasUpacara.jsx
+++ src/components/petugas-upacara/PetugasUpacara.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import './PetugasUpacara.scss';
 
@@ -7,22 +7,85 @@ import Wave from '../../images/waves/Wave Two.png'
 import SecondWave from '../../images/waves/Wave Three.png'
 import Stacks from '../../images/stack-upacara.png'
 import Kupu from '../../images/ananta/kupu.png'
+import LogoOMB2024 from '../../images/Logo OMB 2024 - Biru.png'
 
 // import icons
 import ArrowRight from '../../images/Arrow/Arrow_Right_MD.svg'
 
 // import supergraphics
 import BottomAir from '../../images/supergrafis/Bottom Air.svg'
+import Lebah from '../../images/supergrafis/SUPERGRAFIS METALLIC VIOLET/lebah.svg';
+import Air1 from '../../images/supergrafis/Fill - Air-kanan.svg';
+import Air2 from '../../images/supergrafis/Fill - Air-kiri.svg';
 
 // import components
 import Modal from '../modal/Modal';
+import BoxText from '../box-text/BoxText';
 
 const PetugasUpacara = () => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
   const [showModal, setShowModal] = useState(false);
   
+  const dataSyarat = [
+    [
+      { no: "1", konten: "Mengisi form registrasi.", marginRight: "30px" },
+      { no: "11", konten: "Sehat jasmani dan rohani." },
+    ],
+    [
+      { no: "2", konten: "Mahasiswa aktif Universitas Multimedia Nusantara angkatan 2021-2023.", marginRight: "220px" },
+      { no: "10", konten: "Bersedia mengikuti jadwal pelatihan yang sudah disepakati dengan panitia OMB UMN 2024." },
+    ],
+    [
+      { no: "3", konten: "Minimal IPK 3.0.", marginLeft: "200px" },
+      { no: "9", konten: "Telah dinyatakan lulus dari rangkaian OMB UMN.", marginLeft: "300px" },
+    ],
+    [
+      { no: "4", konten: "Tinggi minimal wanita 160 cm dan laki-laki 165 cm.", marginRight: "130px" },
+      { no: "8", konten: "Mendapatkan perizinan orang tua untuk menjadi petugas upacara OMB UMN 2024.", marginLeft: "130px" },
+    ],
+    [
+      { no: "5", konten: "Postur tubuh tegak dan kaki tidak membentuk O atau X.", marginRight: "120px" },
+      { no: "7", konten: "Memahami Peraturan Baris Berbaris (PBB) dasar.", marginRight: "50px" },
+    ],
+    [
+      { no: "6", konten: "Telah melakukan vaksin ketiga/booster." },
+    ]
+  ];
+
+  const dataSyaratMobile = [
+    {no: "1", konten: "Mengisi form registrasi."},
+    {no: "2", konten: "Mahasiswa aktif Universitas Multimedia Nusantara angkatan 2021-2023."},
+    {no: "3", konten: "Minimal IPK 3.0."},
+    {no: "4", konten: "Tinggi minimal wanita 160 cm dan laki-laki 165 cm."},
+    {no: "5", konten: "Postur tubuh tegak dan kaki tidak membentuk O atau X."},
+    {no: "6", konten: "Telah melakukan vaksin ketiga/booster."},
+    {no: "7", konten: "Memahami Peraturan Baris Berbaris (PBB) dasar."},
+    {no: "8", konten: "Mendapatkan perizinan orang tua untuk menjadi petugas upacara OMB UMN 2024."},
+    {no: "9", konten: "Telah dinyatakan lulus dari rangkaian OMB UMN."},
+    {no: "10", konten: "Bersedia mengikuti jadwal pelatihan yang sudah disepakati dengan panitia OMB UMN 2024."},
+    {no: "11", konten: "Sehat jasmani dan rohani. "}
+  ];
+
   const toggleModal = () => {
     setShowModal(!showModal);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
@@ -64,9 +127,67 @@ const PetugasUpacara = () => {
       </section>
       {showModal && 
         <Modal handleClose={toggleModal}>
-          <div>
-            ISI KONTEN SINI
+
+          {screenSize.width > 560 ? 
+          <div className="modal-syarat">
+            <div className="background-modal-syarat">
+              <img className='logo_omb' src={LogoOMB2024} alt="Logo OMB 2024" />
+            </div>
+            <img className='lebah-judul' src={Lebah} alt="Supergrafis - lebah" />
+            <h2>Syarat Pendaftaran Petugas Upacara OMB UMN 2024</h2>
+            {dataSyarat.map((x, index) => (
+              <div className="baris-card">
+                {x.map((y, index) => (
+                  <>
+                    {y.no > 6 ?
+                      <div className="card" style={{ marginLeft: y.marginLeft, marginRight: y.marginRight }}>
+                        <div className="number-box">
+                          <p>{y.no}</p>
+                        </div>
+                        <BoxText text={y.konten} />
+                      </div>
+                      : y.no < 6 ? <div className="card" style={{ marginLeft: y.marginLeft, marginRight: y.marginRight }}>
+                        <BoxText text={y.konten} />
+                        <div className="number-box">
+                          <p>{y.no}</p>
+                        </div>
+                      </div>
+                        : <div className="card-6">
+                          <div className="number-box">
+                            <p>{y.no}</p>
+                          </div>
+                          <BoxText text={y.konten} />
+                        </div>
+                    }
+                  </>
+                ))}
+              </div>
+            ))}
+            <div className='cta_button'>
+              {/* <img className='air-button-1' src={Air2} alt="Supergrafis - air" /> */}
+              <button className='modal-button'>
+                Daftarkan Dirimu
+              </button>
+              {/* <img className='air-button-2' src={Air1} alt="Supergrafis - air" /> */}
+            </div>
           </div>
+          :
+          <div className="modal-syarat-mobile">
+            <h2>Syarat Pendaftaran Petugas Upacara OMB UMN 2024</h2>
+            {dataSyaratMobile.map((x, index) => (
+              <div className="card-mobile">
+                <div className="number-box">
+                  <p>{x.no}</p>
+                </div>
+                <BoxText text={x.konten} />
+              </div>
+            ))}
+            <button className="button-mobile">
+              <p>Daftarkan Dirimu</p>
+            </button>
+          </div>
+          }
+
         </Modal>
       }
     </>

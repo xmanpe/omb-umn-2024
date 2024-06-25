04fb568af924836c784f9690f93825c0b818f45d
Daffa Akbari <xmanpe04@gmail.com>
2024-06-25 19:52:59 +0700

    Minor updates

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+//import images
+import BungaLotus from '../../images/Outline - Bunga Lotus Biru di Atas Air 1 1.png'
+
+//import icons
+import ArrowRight from '../../images/Arrow/Arrow_Right_MD.png';
+import Wave from '../../images/waves/Wave One.png';
+
+const Header = ({ top, bottom }) => {
+    return (
+        <header className="hero-peserta_section">
+            <img className='bunga-lotus' src={BungaLotus} alt="" />
+            <div className='hero-peserta_content'>
+                <Link to={{ pathname: '/' }} style={{textDecoration: "none"}}>
+                    <div className='back_button' onClick={() => window.history.back()}>
+                        <img src={ArrowRight} alt="Left Arrow" />
+                        <p>Beranda</p>
+                    </div>
+                </Link>
+                <div className='title_divisi'>
+                    <div className='left_thing'>
+                        <div className='literally_title'>
+                            <p>{top}</p>
+                            <h1>{bottom}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img className='wave' src={Wave} alt="The Wave" />
+        </header>
+    );
+}
+ 
+export default Header;

504dfd96891db303118167c6b10184b73ef7c237
JoseAndreasLie <jose.lie2208@gmail.com>
2024-07-03 19:55:44 +0700

    Update InformationModal.jsx
    Update Wordingan Caksana

--- src/components/cards/ann card/information modal/InformationModal.jsx
+++ src/components/cards/ann card/information modal/InformationModal.jsx
@@ -20,7 +20,7 @@ const InformationModal = ({ onClose }) => {
                 <div className='information-content_section'>
                     <h1>Formulir Keikutsertaan OMB UMN 2024</h1>
                     <p>
-                        Formulir Keikutsertaan OMB UMN 2024 merupakan salah satu syarat untuk mengikuti OMB UMN 2024. Seluruh calon Peserta OMB UMN 2024 <b>wajib</b> mengisi Formulir Keikutsertaan OMB UMN 2024 yang mencakup <b>persetujuan orang tua, data diri, dan riwayat kesehatan</b>. 
+                        Formulir Keikutsertaan OMB UMN 2024 merupakan salah satu syarat <b>wajib</b> untuk mengikuti OMB UMN 2024 yang mencakup <b>persetujuan orang tua, data diri, dan riwayat kesehatan</b>. 
                         <br></br> <br></br>
                         Formulir Keikutsertaan OMB UMN 2024 telah dibuka pada <b>Minggu, 30 Juni 2024 pukul 08.00 WIB</b> hingga <b>Minggu, 18 Agustus 2024 pukul 23.59 WIB</b>. 
                         <br></br> <br></br>

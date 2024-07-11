b8e2484515fa273c43d13eb11c2ef0c51789218d
Daffa Akbari <xmanpe04@gmail.com>
2024-07-11 13:24:40 +0700

    Security updates

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -90,7 +90,7 @@ const Footer = () => {
                         <p className='copyright'>© OMB UMN 2024</p>
                         <p className='slebew'>Dikelola oleh <span className="nayanika">Divisi Nayanika<span className="popover">Cie nyariin Nayanika... koornya, ya? 🤭</span></span></p>
                         <br />
-                        <p className='update'>Konten diperbarui per tanggal 8 Juli 2024</p>
+                        <p className='update'>Konten diperbarui per tanggal 11 Juli 2024</p>
                     </div>
                 </div>
             </div>

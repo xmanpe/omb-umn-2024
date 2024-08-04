cf55568de933770d54188f11644e32022bfd4c72
Vinik <56496329+Vn1k@users.noreply.github.com>
2024-08-04 19:21:47 +0700

    Update BingkaiPageRevisi.jsx

--- src/pages/bingkai/BingkaiPageRevisi.jsx
+++ src/pages/bingkai/BingkaiPageRevisi.jsx
@@ -216,7 +216,7 @@ function BingkaiPage() {
 Lotus Biru merajut kisah indah,<br>
 Saya siap untuk awali langkah!<br><br>
     
-Perkenalkan nama saya ${name}, mahasiswa dari program studi ${majors.find(m => m.value === major)?.label} Universitas Multimedia Nusantara 2024. Saya bangga untuk mencetak sejarah baru dengan mengikuti Orientasi Mahasiswa Baru UMN 2024 dan menjadi bagian dari keluarga besar Universitas Multimedia Nusantara.<br>
+Perkenalkan nama saya ${name}, mahasiswa dari program studi ${majors.find(m => m.value === major)?.label} Universitas Multimedia Nusantara 2024. Saya bangga untuk mencetak sejarah baru dengan mengikuti Orientasi Mahasiswa Baru UMN 2024 dan menjadi bagian dari keluarga besar Universitas Multimedia Nusantara.<br><br>
 
 Saya siap untuk tangguhkan ambisi dan menumbuhkan potensi bagi almamater, persada, dan sesama.<br><br>
     
